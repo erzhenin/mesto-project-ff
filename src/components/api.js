c3131fc3aa971bee5c65b81125a140e83e3d0f4f
Andrej Erzhenin <erzhenin.andrej@yandex.ru>
2024-04-08 19:08:41 +0300

    API refactor: moved fetch to separate function

--- src/components/api.js
+++ src/components/api.js
@@ -1,4 +1,4 @@
-const config = {
+const apiConfig = {
   baseUrl: "https://nomoreparties.co/v1/wff-cohort-10",
   headers: {
     authorization: "4988e7fc-55bd-42c1-ae9f-ec575699ec2a",
@@ -6,95 +6,73 @@ const config = {
   },
 };
 
+const checkResponse = (res) => {
+  if (res.ok) {
+    return res.json();
+  }
+
+  return Promise.reject(`Ошибка: ${res.status}`);
+};
+
+const request = (endpoint, options) => {
+  return fetch(apiConfig.baseUrl.concat("/", endpoint), options).then(
+    checkResponse
+  );
+};
+
 const getUserInfo = () => {
-  return fetch(config.baseUrl.concat("/", "users/me"), {
+  return request("users/me", {
     headers: {
       authorization: config.headers.authorization,
     },
-  }).then((res) => {
-    if (res.ok) {
-      return res.json();
-    }
-
-    return Promise.reject(`Ошибка: ${res.status}`);
   });
 };
 
 const setUserInfo = (name, about) => {
-  return fetch(config.baseUrl.concat("/", "users/me"), {
+  return request("users/me", {
     method: "PATCH",
     headers: config.headers,
     body: JSON.stringify({
       name,
       about,
     }),
-  }).then((res) => {
-    if (res.ok) {
-      return res.json();
-    }
-
-    return Promise.reject(`Ошибка: ${res.status}`);
   });
 };
 
 const getInitialCards = () => {
-  return fetch(config.baseUrl.concat("/", "cards"), {
+  return request("cards", {
     headers: {
       authorization: config.headers.authorization,
     },
-  }).then((res) => {
-    if (res.ok) {
-      return res.json();
-    }
-
-    return Promise.reject(`Ошибка: ${res.status}`);
   });
 };
 
 const addCard = (name, link) => {
-  return fetch(config.baseUrl.concat("/", "cards"), {
+  return request("cards", {
     method: "POST",
     headers: config.headers,
     body: JSON.stringify({
       name,
       link,
     }),
-  }).then((res) => {
-    if (res.ok) {
-      return res.json();
-    }
-
-    return Promise.reject(`Ошибка: ${res.status}`);
   });
 };
 
 const deleteCard = (cardId) => {
-  return fetch(config.baseUrl.concat("/", "cards").concat("/", cardId), {
+  return request("cards".concat("/", cardId), {
     method: "DELETE",
     headers: {
       authorization: config.headers.authorization,
     },
-  }).then((res) => {
-    if (res.ok) {
-      return res.json();
-    }
-
-    return Promise.reject(`Ошибка: ${res.status}`);
   });
 };
 
 const changeLike = (cardId, method) => {
-  return fetch(config.baseUrl.concat("/", "cards/likes").concat("/", cardId), {
+  return request("cards/likes".concat("/", cardId), {
     method: method,
     headers: {
       authorization: config.headers.authorization,
     },
-  }).then((res) => {
-    if (res.ok) {
-      return res.json();
-    }
-
-    return Promise.reject(`Ошибка: ${res.status}`);
   });
 };
 
@@ -107,18 +85,12 @@ const removeLike = (cardId) => {
 };
 
 const setAvatar = (avatar) => {
-  return fetch(config.baseUrl.concat("/", "users/me/avatar"), {
+  return request("users/me/avatar", {
     method: "PATCH",
     headers: config.headers,
     body: JSON.stringify({
       avatar,
     }),
-  }).then((res) => {
-    if (res.ok) {
-      return res.json();
-    }
-
-    return Promise.reject(`Ошибка: ${res.status}`);
   });
 };
 
