c74c11862ce247cdced8960e21a4a7ccfa0f1c35
Andrej Erzhenin <erzhenin.andrej@yandex.ru>
2024-04-08 22:55:53 +0300

    Remove excess comments

--- src/components/utils/utils.js
+++ src/components/utils/utils.js
@@ -2,7 +2,6 @@ import { apiConfig } from "./constants";
 
 // Submit functions
 
-// можно сделать универсальную функцию управления текстом кнопки с 3 и 4 необязательными аргументами
 export function renderLoading(isLoading, button, buttonText='Сохранить', loadingText='Сохранение...') {
   if (isLoading) {
     button.textContent = loadingText
@@ -11,28 +10,20 @@ export function renderLoading(isLoading, button, buttonText='Сохранить'
   }
 }
 
-// можно сделать универсальную функцию, которая принимает функцию запроса, объект события и текст во время загрузки
 export function handleSubmit(request, evt, loadingText = "Сохранение...") {
- // всегда нужно предотвращать перезагрузку формы при сабмите
   evt.preventDefault();
 
-  // универсально получаем кнопку сабмита из `evt`
   const submitButton = evt.submitter;
-  // записываем начальный текст кнопки до вызова запроса
   const initialText = submitButton.textContent;
-  // изменяем текст кнопки до вызова запроса
+
   renderLoading(true, submitButton, initialText, loadingText);
   request()
     .then(() => {
-      // любую форму нужно очищать после успешного ответа от сервера
-      // а также `reset` может запустить деактивацию кнопки сабмита (смотрите в `validate.js`)
       evt.target.reset();
     })
     .catch((err) => {
-      // в каждом запросе нужно ловить ошибку
       console.error(`Ошибка: ${err}`);
     })
-    // в каждом запросе в `finally` нужно возвращать обратно начальный текст кнопки
     .finally(() => {
       renderLoading(false, submitButton, initialText);
     });
